99550a60aa3cb7da5c167a3b1943daa144c16f20
Abhishek Ranjan <aviranjan444@gmail.com>
2022-10-17 21:04:00 +0530

    Add solution for problem `B - Permutation Value` Link: https://codeforces.com/contest/1743/problem/B Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1743/submission/176747555

--- codeforces/1743/B.cpp
+++ codeforces/1743/B.cpp
@@ -9,44 +9,35 @@ void solution()
 int n ;
 cin>>n;
 vector<int>v(n,0);
-for(int i=0;i<n-1;++i)
+int temp = 1;
+
+for(int i=0;i<n;i+=2)
 {
-          if(i==0)
-          v[i]=2;
-          else
-          v[i] = v[i-1]+1;
+          v[i] = temp;
+          temp+=1;
+   
 }
 
-v[n-1] = 1;
-// int temp = 1;
-
-// for(int i=0;i<n;i+=2)
-// {
-//           v[i] = temp;
-//           temp+=1;
-   
-// }
-
-// if(n%2==0)
-// {
-//           int j = n-1;
-//           while(j>0)
-//           {
-//                     v[j] = temp;
-//                     temp+=1;
-//                     j-=2;
-//           }
-// }
-// else
-// {
-//           int k = n-2;
-//           while(k>0)
-//           {
-//                     v[k]=temp;
-//                     temp+=1;
-//                     k-=2;
-//           }
-// }
+if(n%2==0)
+{
+          int j = n-1;
+          while(j>0)
+          {
+                    v[j] = temp;
+                    temp+=1;
+                    j-=2;
+          }
+}
+else
+{
+          int k = n-2;
+          while(k>0)
+          {
+                    v[k]=temp;
+                    temp+=1;
+                    k-=2;
+          }
+}
 
 for(auto i:v)
 {
